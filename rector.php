e1c78f80ef735cc7e970a89dd5a5407a91a63f4a
Joey McKenzie <joey.mckenzie27@gmail.com>
2024-06-12 16:38:33 -0700

    chore: wip

--- rector.php
+++ rector.php
@@ -15,7 +15,6 @@ return RectorConfig::configure()
         __DIR__.'/bootstrap',
         __DIR__.'/config',
         __DIR__.'/public',
-        __DIR__.'/resources',
         __DIR__.'/routes',
         __DIR__.'/tests',
         __DIR__.'/database',
